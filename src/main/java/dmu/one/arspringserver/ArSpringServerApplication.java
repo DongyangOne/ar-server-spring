600b3ce525ed20e0c949e02c0bca9f95e5c1640c
pino._.hoo <inhoo23@naver.com>
2022-09-17 11:20:50 +0900

    Create Project

---
+++ src/main/java/dmu/one/arspringserver/ArSpringServerApplication.java
@@ -0,0 +1,13 @@
+package dmu.one.arspringserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArSpringServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArSpringServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dmu/one/arspringserver/ArSpringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dmu.one.arspringserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArSpringServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
